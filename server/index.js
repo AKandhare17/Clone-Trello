2f0c1daadfb8c52aa46c25b0dd4137324561adeb
AtharvaK <atharva2004kandhare@gmail.com>
2026-06-09 12:27:12 +0530

    Remove .env files from GitHub

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://clone-trello-jkpf.onrender.com'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
